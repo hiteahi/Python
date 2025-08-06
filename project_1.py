c07ecbded72e9535742000344920110f9ca14a0c
Hitesh <hitekum9776@gmail.com>
2025-08-06 18:33:02 +0530

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -12,26 +12,25 @@ bot = random.randint(-1, 1)
 
 print(f"")
 
-while True:
-    try:
-            # draw
-        if user == bot:
-            print(f"It's Draw\nyour choose: {user}, bot choose: {bot}")
+try:
+        # draw
+    if user == bot:
+        print(f"It's Draw\nyour choose: {user}, bot choose: {bot}")
 
-            #win
-        elif user == 1 and bot == -1 or user == 0 and bot == 1 or user == -1 and bot == 0:
-            print(f"You Win\nyour choose: {user}, bot choose: {bot}")
+        #win
+    elif user == 1 and bot == -1 or user == 0 and bot == 1 or user == -1 and bot == 0:
+        print(f"You Win\nyour choose: {user}, bot choose: {bot}")
 
-            #lose
-        elif user == 1 and bot == 0 or user == 0 and bot == -1 or user == -1 and bot == 1:
-            print(f"You Lose\nyour choose: {user}, bot choose: {bot}")
+        #lose
+    elif user == 1 and bot == 0 or user == 0 and bot == -1 or user == -1 and bot == 1:
+        print(f"You Lose\nyour choose: {user}, bot choose: {bot}")
 
-            # invalid input
-        else:
-            print(f"Invalid input, please enter 1, 0, or -1.\nyour choose: {user}")
+        # invalid input
+    else:
+        print(f"Invalid input, please enter 1, 0, or -1.\nyour choose: {user}")
 
 
-    except Exception as error:
-        print("samething went wrong, please try again")
+except Exception as error:
+    print("samething went wrong, please try again")
 
         
